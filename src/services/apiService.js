d4f5a026c032f2496736a314d7d8fc0358e9bb55
Thai Bao Duy <thaibaoduy.5724@gmail.com>
2024-10-31 16:59:55 +0700

    part10 Hiển Thị Danh Sách User Phân Trang (Paginate)

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -29,4 +29,14 @@ const deleteUser = (userId) => {
   return axios.delete("v1/participant", { data: { id: userId } });
 };
 
-export { CreateNewUser, getAllUser, putUpdateUser, deleteUser };
+const getUserWithPaginate = (page, limit) => {
+  return axios.get(`v1/participant?page=${page}&limit=${limit}`);
+};
+
+export {
+  CreateNewUser,
+  getAllUser,
+  putUpdateUser,
+  deleteUser,
+  getUserWithPaginate,
+};
